80c2aca37e66b08aa762df7b53235ebbf835847b
skincheg <53021132+skincheg@users.noreply.github.com>
2020-08-14 06:06:01 +0500

    Update ads_importTargetContacts.php

--- functions/ads_importTargetContacts.php
+++ functions/ads_importTargetContacts.php
@@ -6,7 +6,7 @@ require_once ROOT.'/vendor/autoload.php';
 
 use \VK\Client\VKApiClient;
 use \VK\OAuth\Scopes\VKOAuthUserScope;
-$access_token = '2d214538bee9280f20e0646e6764c902e831c5c674b852bde0002cd075ccf148d110be75bae78e548bf49';
+$access_token = '';
 
 $vk = new VKApiClient();
 $response = $vk->ads()->importTargetContacts($access_token, array(
